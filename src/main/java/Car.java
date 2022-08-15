92aac63ecd09a48b7f47825394d5493f3647a597
EP <kjuioqq8@gmail.com>
2022-08-15 13:10:21 +0900

    test: 차의 이름은 5자를 초과할 수 없다

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,7 +3,18 @@ public class Car {
     private int position;
 
     public Car(String name) {
-        this.name = name;
+        String trimName = name.trim();
+        validate(trimName);
+        this.name = trimName;
+    }
+
+    private void validate(String trimName) {
+        if (trimName == null || trimName.length() < 1) {
+            throw new IllegalArgumentException("자동차 이름은 빈 문자열이 올 수 없습니다.");
+        }
+        if (trimName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 
     public String name() {
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -2,10 +2,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+/**
+ * # 2. 자동차 경주 게임 구현
+ *
+ * 기능 요구사항
+ * - 각 자동차에 이름을 부여할 수 있다. 자동차 이름은 5자를 초과할 수 없다.
+ *   - 자동차는 이름이 있다.
+ * 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
+ *   - 자동차는 위치가 있다.
+ * - 자동차 이름은 쉼표(,)를 기준으로 구분한다.
+ * - 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.
+ * - 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한명 이상일 수 있다.
+ */
 public class CarTest {
 
     private String carName = "ep";
+
+    @Test
+    @DisplayName("차의 이름은 5자를 초과할 수 없다")
+    void car_name_less_than_5() {
+        String carNameLengthMoreThan5 = "east_person";
+        assertThatThrownBy(() -> new Car(carNameLengthMoreThan5)).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("차의 초기 위치값은 0이다")
